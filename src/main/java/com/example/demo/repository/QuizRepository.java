8d6ec267c710bcc03be1866dba178a53a290c38b
shreemanvi <shreemanvi.05@gmail.com>
2026-08-31 20:49:44 +0530

    Delete src directory

--- src/main/java/com/example/demo/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Quiz;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizRepository extends MongoRepository<Quiz, String> {
-}
--- src/main/java/com/example/demo/repository/QuizSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.QuizSession;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizSessionRepository extends MongoRepository<QuizSession, String> {
-}
